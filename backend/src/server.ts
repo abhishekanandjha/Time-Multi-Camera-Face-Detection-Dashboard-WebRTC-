610e220a97cbbb9d073e77d86ff5937fd43d0733
Abhishek Anand Jha <43599214+abhishekanandjha@users.noreply.github.com>
2025-09-24 10:05:42 +0530

    -

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono';
 import authRoutes from './routes/auth.routes';
 import { authMiddleware } from './middlewares/auth';
+import cameraRoutes from './routes/camera.routes';
 
 const app = new Hono();
 
 app.route('/api/auth', authRoutes);
+app.route('/api/cameras', cameraRoutes)
+
 
 // protected route example
 app.get('/api/protected', authMiddleware, (c) => {
